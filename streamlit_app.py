5a7c417b23e432a86f42a187fdb51cee41721cad
KavitaSelkery <103625378+KavitaSelkery@users.noreply.github.com>
2022-08-17 19:48:27 +0530

    Update streamlit_app.py
    added a menu item and added emojis.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.subheader('Omega 3 & Blueberry Oatmeal')
-streamlit.subheader('Kale, Spinach & Rocket Smoothie')
-streamlit.subheader('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥚Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avocado Toast')
+streamlit.header('🍌🥭Build Your Own Fruit Smoothie🥝')
