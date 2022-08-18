6e6bc01d55e29291fd666ecdc1b0efe312fc48be
KavitaSelkery <103625378+KavitaSelkery@users.noreply.github.com>
2022-08-18 20:37:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,3 +39,8 @@ my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
+
+fruit_choice1 = streamlit.text_input('Which fruit would you like?','Kiwi')
+streamlit.write('The user entered ', fruit_choice1)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
+#streamlit.text(fruityvice_response.json())
